620243600174e9fc116868bf106b5253bb1b853a
phamhoangtuan <phamhoangtuan.it@gmail.com>
2020-03-15 21:31:37 +0800

    Update css, update professional experience, education

--- src/metadata/metadata.js
+++ src/metadata/metadata.js
@@ -4,67 +4,42 @@ module.exports = {
   baseUrl: 'https://phamhoangtuan.github.io/cv',
   facts: {
     'Residence': 'Singapore',
-    'LinkedIn': `<a href="https://www.linkedin.com/in/phamhoangtuan">
-                  <span class="print">https://www.linkedin.com/in/phamhoangtuan</span>
-                  <span class="screen">https://www.linkedin.com/in/phamhoangtuan</span>
-                </a>`,
+    'LinkedIn': `<a href="https://www.linkedin.com/in/phamhoangtuan">https://www.linkedin.com/in/phamhoangtuan</span></a>`,
     'Email': '<a href="mailto:phamhoangtuan.it@gmail.com">phamhoangtuan.it@gmail.com</a>',
     'Languages': 'Vietnamese, English'
   },
-  skills: [
-    ['Comedy', '++++'],
-    ['Love', '++++'],
-    ['Theater', '++++'],
-    ['Tragedy', '++++'],
-    ['Poetry', '++++'],
-    ['Erotic', '+++'],
-    ['Sonnets', '++++'],
-    ['Plays', '++++'],
-    ['Writing', '++++'],
-    ['Directing', '+++'],
-    ['Literature', '++++'],
-    ['Influence', '++++'],
-  ],
-  plays: [
+  experiences: [
     {
-      title: 'Hamlet',
-      written: 'Likely early 17th century',
-      badges: ['tragedy', 'philosophy', 'death'],
-      contents: `
-Prince Hamlet is visited by his father's ghost and ordered to avenge his father's murder by killing King Claudius, his uncle. After struggling with several questions, including whether what the ghost said is true and whether it is right for him to take revenge, Hamlet, along with almost all the other major characters, is killed.
-`
+      company: 'SP Digital',
+      location: 'Singapore',
+      title: 'Senior Data Engineer',
+      period: 'Feb 2019 - Present',
+      skills: ['Python', 'Java', 'Talend', 'Hadoop', 'Spark'],
+      contents: ``
     },
     {
-      title: 'Twelfth Night',
-      written: '1600 – 1601',
-      badges: ['comedy', 'love'],
-      contents: `
-Viola finds herself shipwrecked in Illyria and, assuming that her brother Sebastian has died in the wreck, disguises herself as a man to gain a position in Duke Orsino's court. Orsino sends Viola (whom he knows as Cesario) to deliver a message to his love, Olivia. Olivia, however, dislikes the Duke. She falls in love with Viola, who she thinks is a man. Eventually, Viola's brother Sebastian, who in fact was unharmed in the wreck, reappears. At a critical moment, Viola's true identity is revealed when members of the court notice the similarities between her and Sebastian. Olivia quickly falls in love with Sebastian, and Viola confesses her love for the Duke.
-`
+      company: 'Honestbee',
+      location: 'Singapore',
+      title: 'Data Engineer',
+      period: 'Sep 2016 - Jan 2019',
+      skills: ['Python', 'AWS', 'Airflow', 'Postgres', 'Docker', 'Looker'],
+      contents: ``
     },
     {
-      title: 'The Tragedy of Romeo and Juliet',
-      written: '1595–1596, with a possible early draft written in 1591',
-      badges: ['tragedy', 'death', 'love'],
-      contents: `
-In Verona, Italy, two families, the Montagues and the Capulets, are in the midst of a bloody feud. Romeo, a Montague, and Juliet, a Capulet, fall in love and struggle to maintain their relationship in the face of familial hatred. After Romeo kills Juliet's cousin Tybalt in a fit of passion, things fall apart. Both lovers eventually commit suicide within minutes of each other, and the feuding families make peace over their recent grief.
-`
-    }
-  ],
-  experience: [
-    {
-      header: 'Performance',
-      skills: ['theater', 'play'],
-      contents: `
-Thousands of performances of my plays have been staged since the end of the 16th century. Many of my greatest plays were performed by the Lord Chamberlain's Men and King's Men acting companies at the [Globe](https://en.wikipedia.org/wiki/Globe_Theatre) and Blackfriars Theatres.
-`
+      company: 'Crayon Data',
+      location: 'Singapore',
+      title: 'Data Scientist Intern',
+      period: 'Aug 2015 - Mar 2016',
+      skills: ['Python', 'Java', 'MySQL'],
+      contents: ``
     },
     {
-      header: 'Poetry',
-      skills: ['poetry', 'erotic'],
-      contents: `
-In 1593 and 1594, when the theatres were closed because of plague, I published two narrative poems on sexual themes, Venus and Adonis and The Rape of Lucrece. He dedicated them to Henry Wriothesley, Earl of Southampton.
-`
+      company: 'FPT Software',
+      location: 'Ho Chi Minh City, Vietnam',
+      title: 'Software Engineer',
+      period: 'Jun 2013 - Dec 2014',
+      skills: ['C#', 'Sencha Touch', 'Silverlight'],
+      contents: ``
     }
   ]
 };
