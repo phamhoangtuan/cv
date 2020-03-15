40ee4165799cd5ffc5a90219d95b4f94a3db59c3
phamhoangtuan <phamhoangtuan.it@gmail.com>
2020-03-16 01:07:41 +0800

    Add content in Honestbee, Crayon, FPT

--- src/metadata/metadata.js
+++ src/metadata/metadata.js
@@ -14,16 +14,26 @@ module.exports = {
       location: 'Singapore',
       title: 'Senior Data Engineer',
       period: 'Feb 2019 - Present',
-      skills: ['Python', 'Java', 'Talend', 'Hadoop', 'Spark'],
-      contents: ``
+      skills: ['Python', 'Java', 'Talend', 'Hadoop', 'Hive', 'Spark', 'HBase'],
+      contents: `
+- 
+      `
     },
     {
       company: 'Honestbee',
       location: 'Singapore',
       title: 'Data Engineer',
       period: 'Sep 2016 - Jan 2019',
-      skills: ['Python', 'AWS', 'Airflow', 'Postgres', 'Docker', 'Looker'],
-      contents: ``
+      skills: ['Python', 'AWS', 'Airflow', 'Postgres', 'Docker', 'Mesos', 'Looker'],
+      contents: `
+- Developed a data pipeline to collect users' orders information, users' behaviors and other third parties data, process them and store in Datawarehouse. As a result, BI team always have clean and accuracy data to show in their reports.
+- Optimized queries to increase overall performance by 25% for product ranking and recommendation system.
+- Maitained legacy data infrastructure and ensure its high availability for other dependent data services.
+      `,
+      review: {
+        file: 'review_honestbee.png',
+        author: 'Nick Young, Head of Data'
+      }
     },
     {
       company: 'Crayon Data',
@@ -31,15 +41,25 @@ module.exports = {
       title: 'Data Scientist Intern',
       period: 'Aug 2015 - Mar 2016',
       skills: ['Python', 'Java', 'MySQL'],
-      contents: ``
+      contents: `
+- Developed web crawler with Python libraries to collect data automatically from e-commerce website. As a result, data team can have many raw data to process and train data models for product improvement.
+- Implemented a novel extractor to retrieve websites' information based on HTML structure in under 5 seconds per website.
+      `,
+      review: {
+        file: 'review_crayon.png',
+        author: 'Vivek Rensol, Product Director'
+      }
     },
     {
       company: 'FPT Software',
       location: 'Ho Chi Minh City, Vietnam',
       title: 'Software Engineer',
       period: 'Jun 2013 - Dec 2014',
-      skills: ['C#', 'Sencha Touch', 'Silverlight'],
-      contents: ``
+      skills: ['C#', 'Javascript', 'Sencha Touch', 'Silverlight'],
+      contents: `
+- Developed a mobile app that useful for salemans to track their performance and reduce paper works. Customers' feedback is 80% positive in general.
+- Improved web portal legacy code with Javascipt best practices to increase API response time by 50%.
+      `
     }
   ]
 };
